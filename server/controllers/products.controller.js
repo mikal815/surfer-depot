437980e7f1ad5b23e8d1dda41b67565d379e4b37
Mike M <mikal815@users.noreply.github.com>
2021-06-25 23:39:13 -0400

    POST product to database

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,8 +1,24 @@
 const { productsService } = require('../services')
 
 const productsController = {
-
+    async addProduct(req, res, next) {
+        try {
+            const product = await productsService.addProduct(req.body);
+            res.json(product)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
+// {
+//     "model":"Dice Game",
+//     "brand":"6049a1ad9c9a2615b86c04f2",
+//     "description":"This is the content of the post",
+//     "price":239,
+//     "available":19,
+//     "itemsSold":10,
+//     "shipping":true
+// }
 
 module.exports = productsController
