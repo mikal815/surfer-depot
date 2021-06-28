f567a05a6de82389bccbc40d13a5f039372bd1b3
Mike M <mikal815@users.noreply.github.com>
2021-06-27 20:57:44 -0400

    Connect Redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,15 @@ import Routes from './routes';
 
 import 'resources/styles/styles.css';
 
+import { Provider } from 'react-redux';
+import ReduxStore from './store';
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={ReduxStore()}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
