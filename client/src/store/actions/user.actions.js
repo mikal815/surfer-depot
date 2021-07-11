dd7352911feb0af58cce6f1455e2c489d7265a53
Mike M <mikal815@users.noreply.github.com>
2021-07-11 19:59:25 -0400

    Dispatch the cart

--- client/src/store/actions/user.actions.js
+++ client/src/store/actions/user.actions.js
@@ -97,4 +97,21 @@ export const userChangeEmail = (data) => {
             dispatch(actions.errorGlobal(error.response.data.message))
         }
     }
+}
+
+
+
+export const userAddToCart = (item) => {
+    return async (dispatch, getState) => {
+        try {
+            const cart = getState().users.cart;
+            dispatch(actions.userAddToCart([
+                ...cart,
+                item
+            ]))
+            dispatch(actions.successGlobal(`${item.model} added to cart :)`))
+        } catch (error) {
+            dispatch(actions.errorGlobal(error.response.data.message))
+        }
+    }
 }  
