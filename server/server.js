f44d8c74faad39ace90dae404932c59d906be112
Mike M <mikal815@users.noreply.github.com>
2021-07-14 21:15:01 -0400

    Prepare deploy

--- server/server.js
+++ server/server.js
@@ -42,6 +42,13 @@ app.use((err, req, res, next) => {
 })
 
 
+app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    });
+}
 
 
 const port = process.env.PORT || 3001
